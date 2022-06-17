a2cb637915e1905bd3d7e959b157904a1b572931
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-06-17 19:37:47 +0100

    feat: sub menu

--- components/common/Navbar/components/MobileNavbar.tsx
+++ components/common/Navbar/components/MobileNavbar.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import styles from './../Navbar.module.css';
 import { useUIContext } from '@components/ui/Context';
 import { menuLinks } from '../data/linklist.data';
-import { MenuListType } from '../model/LinkListType';
+import { LinkType, MenuListType, SubMenuType } from '../model/LinkListType';
 import { ArrowLeft, ChevronRight } from '@components/icons';
 import cx from 'clsx';
 import { useState } from 'react';
@@ -11,13 +11,16 @@ const MobileNavbar = () => {
   const { toggleSidebar, displaySidebar } = useUIContext();
   const [closeMainMenu, setCloseMainMenuState] = useState<boolean>(true);
   const [closeSubMainMenu, setCloseSubMainMenuState] = useState<boolean>(false);
-  const openSubMenu = () => {
+  const [subMenuDataIdx, setSubMenuDataIdx] = useState<number>(-1);
+  const openSubMenu = (idx: number) => {
     setCloseMainMenuState(false);
     setCloseSubMainMenuState(true);
+    setSubMenuDataIdx(idx);
   };
   const closeSubMenu = () => {
     setCloseMainMenuState(true);
     setCloseSubMainMenuState(false);
+    setSubMenuDataIdx(-1);
   };
   return (
     <div className="w-full fixed top-0 left-0 right-0">
@@ -53,14 +56,13 @@ const MobileNavbar = () => {
         <>
           {closeMainMenu && (
             <div className="w-full">
-              {menuLinks.map((menuLink: MenuListType) => (
+              {menuLinks.map((menuLink: MenuListType, index: number) => (
                 <div
                   className="w-full flex flex-row items-center py-[8px]"
                   key={menuLink.id}
-                  onClick={openSubMenu}
+                  onClick={() => openSubMenu(index)}
                 >
                   <span className="mr-[4px] font-secondary font-bold uppercase">
-                    {' '}
                     {menuLink.label}
                   </span>
                   <span>
@@ -74,12 +76,37 @@ const MobileNavbar = () => {
         <>
           {closeSubMainMenu && (
             <div className="w-full">
-              <div>
+              <div className="flex flex-row items-center cursor-pointer">
                 <button className="border-0" onClick={closeSubMenu}>
                   <ArrowLeft />
                 </button>
+                <span className="font-primary text-base ml-[8px]">Back</span>
               </div>
-              submenu
+              <>
+                {menuLinks[subMenuDataIdx] && (
+                  <div className="w-full">
+                    <h2 className="uppercase font-bold my-[24px]">
+                      {menuLinks[subMenuDataIdx]?.label}
+                    </h2>
+                    <div>
+                      {menuLinks[subMenuDataIdx]?.subMenu.map(
+                        (subMenu: SubMenuType) => (
+                          <div className="w-full py-[8px]" key={subMenu.id}>
+                            <h2 className="font-bold">{subMenu.label}</h2>
+                            <ul className="pl-[16px]">
+                              {subMenu.links.map((link: LinkType) => (
+                                <li key={link.id} className="py-[8px]">
+                                  {link.label}
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+                        )
+                      )}
+                    </div>
+                  </div>
+                )}
+              </>
             </div>
           )}
         </>
