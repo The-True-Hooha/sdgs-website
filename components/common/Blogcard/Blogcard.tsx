aa32bdd1b443c076ecd07507e38941eb4ad11f31
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-07-02 17:27:01 +0100

    feat: intro section

--- components/common/Blogcard/Blogcard.tsx
+++ components/common/Blogcard/Blogcard.tsx
@@ -1,8 +1,11 @@
-
-
 const Blogcard = () => {
-  
-}
-
+  return (
+    <div>
+      <div className="flex flex-row">
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
 export default Blogcard;
---
+++ components/common/Intro/Intro.tsx
@@ -0,0 +1,2 @@
+const Intro = () => {};
+export default Intro;
