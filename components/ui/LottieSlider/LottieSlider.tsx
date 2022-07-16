0ccb24bbc75e0cf03553146243e81e6c8dcf058d
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-07-16 18:16:49 +0100

    feat: lottie files and sdgs-goals imagees

--- components/ui/LottieSlider/LottieSlider.tsx
+++ components/ui/LottieSlider/LottieSlider.tsx
@@ -1,35 +1,50 @@
-import { useEffect, useRef, useState } from 'react';
-import { Player, Controls } from '@lottiefiles/react-lottie-player';
-
+import { useEffect, useRef, useState, useCallback } from 'react';
+import { Player } from '@lottiefiles/react-lottie-player';
+import { motion, AnimatePresence } from 'framer-motion';
 const LottieSlider = () => {
-  const container = useRef<any>(null);
   const useEffectExecuted = useRef<boolean>(false);
   const setTimeOutInstance = useRef<any>(null);
   const [counter, setCounter] = useState<any>(1);
-
-  useEffect(() => {
+  const [isAnimating, setIsAnimating] = useState<Boolean>(true);
+  const handleCounter = useCallback(() => {
     if (useEffectExecuted.current) {
-      const lottieSliderJson = () => {
-        if (counter > 1) {
+      try {
+        // setIsAnimating(false);
+        if (counter > 2) {
           setCounter(0);
         }
+        // console.log('ss', isAnimating);
+        // setIsAnimating(true);
+        // console.log('rrrr', isAnimating);
         setCounter((prev: number) => prev + 1);
-      };
-      setTimeOutInstance.current = setTimeout(lottieSliderJson, 5000);
+      } catch (error) {}
+    }
+  }, [counter]);
+  useEffect(() => {
+    if (useEffectExecuted.current) {
+      setTimeOutInstance.current = setInterval(handleCounter, 8000);
     }
     // the usual logic to be used in the useEffect hook
     useEffectExecuted.current = true;
-    return () => clearTimeout(setTimeOutInstance.current);
-  }, [counter]);
+    return () => clearInterval(setTimeOutInstance.current);
+  }, [handleCounter]);
   return (
-    <div ref={container}>
-      <Player
-        autoplay
-        loop
-        src={require(`./Lottie-json/data-${counter - 1}.json`)}
-        style={{ height: '300px', width: '300px' }}
-      ></Player>
-    </div>
+    <AnimatePresence>
+      {isAnimating && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={ { opacity: 0 } }
+        >
+          <Player
+            autoplay
+            loop
+            src={require(`./Lottie-json/data-${counter - 1}.json`)}
+            style={{ height: '300px', width: '300px' }}
+          ></Player>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
