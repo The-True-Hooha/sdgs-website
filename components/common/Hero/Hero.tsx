48a794921dcbed5befcee80350fc42911e1379d3
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-07-28 22:41:48 +0100

    refactor: hero

--- components/common/Hero/Hero.tsx
+++ components/common/Hero/Hero.tsx
@@ -1,18 +1,29 @@
-import { LottieSlider } from '@components/ui';
 import Image from 'next/image';
-const Hero = (): JSX.Element => {
+import React from 'react';
+type HeroType = {
+  label?: string;
+  bgImgUrl?: string;
+  overlayBgCss?: string;
+};
+
+const Hero: React.FunctionComponent<HeroType> = ({
+  label = 'Page label',
+  bgImgUrl = '/assets/images/for-sdgs.jpeg',
+}): JSX.Element => {
   return (
     <div className="w-full h-auto">
-      <div className="w-full h-screen relative overflow-hidden flex items-center">
+      <div className="w-full h-[50vh] relative overflow-hidden flex items-center">
         <div className="w-full h-full bg-black/30 absolute z-10"></div>
-        <div className='absolute z-20'>
-          <p>SDGS FUPRE</p>
-          <h2 className="text-white font-secondary font-bold uppercase text-4xl">Contact Us</h2>
+        <div className="absolute z-20 p-[50px] flex flex-col gap-[30px]">
+          <p className="text-white font-secondary text-2xl">SDGS FUPRE</p>
+          <h2 className="text-white font-secondary font-bold uppercase text-5xl">
+            {label}
+          </h2>
         </div>
         <Image
-          src="/assets/images/for-sdgs.jpeg"
+          src={bgImgUrl}
           alt="main-logo"
-          className="object-cover object-center"
+          className="object-cover object-top"
           layout="fill"
           priority
         />
