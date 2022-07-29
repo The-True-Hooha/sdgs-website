d25d76fd883610e405a822ec87cf850ad2615637
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-07-29 09:21:21 +0100

    feat: server error (500) page implemented

--- pages/500.tsx
+++ pages/500.tsx
@@ -3,14 +3,17 @@ import Link from 'next/link';
 
 export default function FourOhFour() {
   return (
-    <Layout cssClasses="grid place-items-center min-h-[500px] h-[80vh]" useHero={false}>
+    <Layout
+      cssClasses="grid place-items-center min-h-[500px] h-[80vh]"
+      useHero={false}
+    >
       <div className="flex flex-col items-center">
         <h1 className="font-secondary text-2xl">
-          <span className="font-bold">404 </span>| Page Not Found
+          <span className="font-bold">500 </span>| Server-side error occurred
         </h1>
-        <Link href="/">
+        <Link href="mailto:ereyomioluwaseyi@gmail.com">
           <a className="font-secondary text-md p-2 block bg-red-400 hover:bg-red-600 rounded-lg text-white">
-            Go back home
+            Contact our Admin about this
           </a>
         </Link>
       </div>
