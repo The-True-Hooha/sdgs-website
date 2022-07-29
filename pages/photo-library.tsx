29f6eb6b54a45f51cb2d7f9b52d0afb5ab8e48dc
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-07-29 09:04:55 +0100

    feat: sdgs pictures

---
+++ pages/photo-library.tsx
@@ -0,0 +1,18 @@
+import Layout from '@components/common/layout';
+import { NextPage } from 'next';
+
+const PhotoGallery: NextPage = () => {
+  return (
+    <Layout
+      heroDetails={{
+        label: 'Photo Gallery',
+      }}
+    >
+      <div className="w-full p-9">
+        
+      </div>
+    </Layout>
+  );
+};
+
+export default PhotoGallery;
