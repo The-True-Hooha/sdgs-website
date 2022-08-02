73d890ce412a6194e5cd1095ff7c0001f6d04dc6
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-08-02 17:37:25 +0100

    refactor: export getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = ( { allPosts, preview }: any ) => {
 
 export default Home;
 
-export async function getStaticProps({ preview = false }) {
+async function getStaticProps({ preview = false }) {
   const allPosts = await getAllPostsForHome(preview);
 
   return {
