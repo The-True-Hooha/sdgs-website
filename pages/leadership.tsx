093f60d2e71a3d80dda7383482be87ad480d1fcd
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-08-14 19:50:36 +0100

    refactor: leadership page

---
+++ pages/leadership.tsx
@@ -0,0 +1,23 @@
+import Layout from '@components/common/layout';
+
+const leadershipData = [
+  {
+    id: 1,
+    name: 'Dr. Elias Emeka Elemike',
+    image: '/images/leadership/dr-elemike.jpg',
+  },
+];
+export default function Leadership() {
+  return (
+    <Layout
+      cssClasses="grid place-items-center min-h-[500px] h-[80vh]"
+      useHero={false}
+    >
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="font-secondary text-2xl text-center">
+         Leader ship
+        </h1>
+      </div>
+    </Layout>
+  );
+}
