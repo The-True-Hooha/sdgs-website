148b5633737ea531bb41dba6a86929dcbd4e4a2d
David Ogar <owogogahhero@outlook.com>
2022-08-16 12:36:59 +0100

    fix typo

--- pages/leadership.tsx
+++ pages/leadership.tsx
@@ -10,13 +10,18 @@ const leadershipData = [
 export default function Leadership() {
   return (
     <Layout
-      cssClasses="grid place-items-center min-h-[500px] h-[80vh]"
+      // cssClasses="grid place-items-center min-h-[500px] h-[80vh]"
       useHero={false}
     >
-      <div className="flex flex-col items-center justify-center">
+      <div className="">
+        <div className=''>
+        <p>HOME / OUR LEADERSHIP</p>
+      {/* <div className="flex flex-col items-center justify-center">
         <h1 className="font-secondary text-2xl text-center">
          Leader ship
         </h1>
+      </div> */}
+      </div>
       </div>
     </Layout>
   );
