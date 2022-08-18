7a9a12165ca0f9aa605d78e01267984cc1a6d426
David Ogar <owogogahhero@outlook.com>
2022-08-18 18:59:43 +0100

    styling data

--- pages/leadership.tsx
+++ pages/leadership.tsx
@@ -1,6 +1,7 @@
 import Layout from '@components/common/layout';
 import Image from 'next/image';
 import Link from 'next/link';
+import { BsArrowRight } from 'react-icons/bs'
 import Elemike from '../public/assets/images/leadership/dr-elemike.jpg'
 
 export type LeadershipDataT = {
@@ -13,44 +14,44 @@ export type LeadershipDataT = {
 export const leadershipCardData: LeadershipDataT[] = [
   {
     id: 1,
-    name: "Dr Akinyemi Olufemi Ogunkeyede",
+    name: "Akinyemi O. Ogunkeyede",
     image: '/assets/images/leadership/dr-akinyemi.jpg',
     title: "Director FUPRE-SDSN",
 
   },
   {
     id: 2,
-    name: 'Dr (Mrs.) Doris Fovwe Ogeleka',
+    name: 'Doris F. Ogeleka',
     image: '/assets/images/leadership/prof-dori.jpg',
     title: "Pioneer chair, Sustainable Development Solutions Network (SDSN)"
   },
   {
     id: 3,
-    name: 'Dr Mrs. Omosede E. Osafile',
+    name: 'Omosede E. Osafile',
     image: '/assets/images/leadership/prof-osafile.jpg',
     title: "Ag. Director Sub-Degree Program FUPRE"
   },
   {
     id: 4,
-    name: 'Dr. (Mrs) Laurelta Esivweneta Tudararo-Aherobo (MNES, MASM, MOWSD)',
+    name: 'Laurelta Esivweneta T.',
     image: '/assets/images/leadership/laurelta.jpg',
     title: "Acting Director of the Integrated Institute of Environment and Development (IIED), FUPRE."
   },
   {
     id: 5,
-    name: 'Dr. (Mrs). Ogwuche Christiana Ene',
+    name: 'Ogwuche C. Ene',
     image: '/assets/images/leadership/ogwuche.jpg',
     title: "Coordinator of the Entrepreneurship Group of FUPRE-SDSN"
   },
   {
     id: 6,
-    name: 'Dr Adedoyin A.Adebayo',
+    name: 'Adedoyin A. Adebayo',
     image: '/assets/images/leadership/adedoyin.jpg',
     title: "Advocate of the 17 SDG goals especially climate matters, gender equality and partnerships for goals"
   },
   {
     id: 7,
-    name: 'Dr Suraju O. Ogundele',
+    name: 'Suraju O. Ogundele',
     image: '/assets/images/leadership/suraju.jpg',
     title: "Coordinator of Youth Development and Skills Thematic Group and Quiz Master of the SDSN"
   },
@@ -62,19 +63,19 @@ export const leadershipCardData: LeadershipDataT[] = [
   },
   {
     id: 9,
-    name: 'Mrs Geraldine Omonzusi Okojie',
+    name: 'Geraldine O. Okojie',
     image: '/assets/images/leadership/geraldine.jpg',
     title: "Administrative officer of FUPRE SDSN"
   },
   {
     id: 10,
-    name: 'Mrs. Akpobassaha O. Peace',
+    name: 'Akpobassaha O. Peace',
     image: '/assets/images/leadership/peace.jpg',
     title: "Physical planner and Developer, a Civil and Structural Engineer"
   },
   {
     id: 11,
-    name: 'Mrs. Nwachukwu Bekenawei Endurance',
+    name: 'Nwachukwu B. Endurance',
     image: '/assets/images/leadership/bekenawei.jpg',
     title: "Keen in educational development of youths"
   },
@@ -86,7 +87,7 @@ export const leadershipCardData: LeadershipDataT[] = [
   },
   {
     id: 13,
-    name: 'Mrs Makun Omowumi Jayeola',
+    name: 'Makun O. Jayeola',
     image: '/assets/images/leadership/makun.jpg',
     title: "Senior Technologist in the Department of Environmental Management and Toxicology, FUPRE"
   }
@@ -96,7 +97,7 @@ export const leadershipCardData: LeadershipDataT[] = [
 const leadership = () => {
   return (
     <Layout
-    cssClasses='max-h-[3000px]'
+    cssClasses='h-[4500px]'
       useHero={false}
     >
       <div className="">
@@ -133,21 +134,28 @@ const leadership = () => {
           Institute of Chattered Chemists of Nigeria (ICCON), South African Chemical Institute (SACI) and Royal Society of Chemistry (RSC).
           <span>Contact:+2348035642445,elemike.elias@fupre.edu.ng,chemphilips@yahoo.com</span>
           </p>
-          <Image className='' alt={"dr elemike"} src={Elemike} width={""} height={""}/>
+          <div><Image className='' alt={"dr elemike"} src={Elemike} width={200} height={200}/></div>
           </div>
         </div>
 
-        <div className='mt-[20px] flex flex-wrap justify-start gap-[10px]'>
+        <div className='mt-[20px] justify-start grid grid-cols-3 gap-[7px]'>
           {leadershipCardData.map((v: LeadershipDataT, idx: number) => (
-            <div key={idx} className="w-[100px] h-[100px] mt-6">
+            <div key={idx} className="w-[350px] h-[550px] bg-[#f7f7f7] mt-6">
               <Image
               src={v.image}
               alt={v.name}
-              width={10}
-              height={10}
+              width={350}
+              height={380}
               layout="fixed"
+              className='mt-[50px] rounded'
               />
-              {v.name}{v.title}
+              <div className='mt-4 font-secondary text-[15px] flex justify-center'>{v.name}</div>
+              <div className='mt-4 font-primary text-[15px] ml-6'>{v.title}</div>
+              <div className='flex w-full justify-start mt-4 bg-red-500'>
+                <Link href="/">
+                <a>READ MORE<BsArrowRight className=''/></a>
+                </Link>
+              </div>
             </div>
           ))}
         </div>
